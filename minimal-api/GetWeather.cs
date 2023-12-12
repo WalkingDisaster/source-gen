cea4501ff20d4fe0efec4f5946b930f340a0a07d
Michael Meadows <michaelmeadows@gmail.com>
2023-12-12 13:46:30 -0500

    Added logging

--- minimal-api/GetWeather.cs
+++ minimal-api/GetWeather.cs
@@ -2,12 +2,21 @@
 
 internal class GetWeather
 {
+    private readonly ILogger<GetWeather> _logger;
+
     string[] _summaries = {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
-    
+
+    public GetWeather(ILogger<GetWeather> logger)
+    {
+        _logger = logger;
+    }
+
     internal async Task<WeatherForecastResults> GetWeatherFor(string city)
     {
+        GetWeatherClassLogging.GetForCity(_logger, city);
+
         return await Task.FromResult(new WeatherForecastResults(
             city,
             Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -17,4 +26,10 @@ internal class GetWeather
                 _summaries[Random.Shared.Next(_summaries.Length)]
             )).ToArray())).ConfigureAwait(false);
     }
+}
+
+internal static partial class GetWeatherClassLogging
+{
+    [LoggerMessage(1, LogLevel.Information, "Getting weather for city {city}", EventName = "GetWeatherForCity")]
+    internal static partial void GetForCity(ILogger logger, string city);
 }
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,8 +1,10 @@
-using System.Text.Json.Serialization.Metadata;
 using System.Text.Json;
 using WalkingDisaster.SourceGenExamples.MinimalApi;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging(x => x.AddSimpleConsole());
+
 builder.Services
     .ConfigureHttpJsonOptions(opt =>
     {
@@ -21,12 +23,5 @@ app.UseHttpsRedirection();
 
 app.MapGet("/weatherforecast", async (GetWeather service) => await service.GetWeatherFor("Generic").ConfigureAwait(false));
 app.MapPost("/weatherforecast", async (GetWeather service, WeatherForecastRequest request) => await service.GetWeatherFor(request.City).ConfigureAwait(false));
-
-app.MapPost("/weatherforecast2", async (GetWeather service, HttpRequest request) =>
-{
-    var req = await JsonSerializer.DeserializeAsync(request.Body, SourceGenerationContext.Default.WeatherForecastRequest).ConfigureAwait(false);
-    var result = await service.GetWeatherFor(req.City).ConfigureAwait(false);
-    return JsonContent.Create(JsonSerializer.Serialize(result, SourceGenerationContext.Default.WeatherForecastResults));
-});
 
 app.Run();
